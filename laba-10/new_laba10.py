46fc99c119174a1b844f176c79c8cd6afff91551
NOVLOST <159267939+NOVLOST@users.noreply.github.com>
2024-10-01 23:43:51 +0400

    Update new_laba10.py

--- laba-10/new_laba10.py
+++ laba-10/new_laba10.py
@@ -5,71 +5,66 @@ from tkinter import ttk
 
 
 
-HEIGHT = 800
+HEIGHT = 800 # размер окна
 WIDTH = 600
 
-comp_sigh = 'o'
+comp_sigh = 'o' # знаки игрока и компьютера
 human_sigh = 'x'
 
-count_steps = 0
+count_steps = 0 # счетчик количества ходов
 
+# координаты по которым раставляются знаки
+board_coord_colect = ((200,100),(350,100),(500,100),(200,250),(350,250),(500,250),(200,400),(350,400),(500,400))
 
+# если game_over == True то выходит сообщение о "GAME OVER!!!"
 game_over_flag = False
+
+#флаг для костыля чтобы он использовался только один раз
 fix_me_flag = True
-class Window():
+class Window(): # класс отвечает за построение окна , за сообщениея "GAME OVER" , действия при нажатии игрока на мышку
 
     def __init__(self,height,width):
 
-        self.height = height
+        self.height = height #ширина и высота
         self.width = width
         self.root = Tk()
         self.root.geometry(f"{self.height}x{self.width}+200+200")
         self.root.resizable(False, False)
-        self.can_play_flag = True
-
-
 
 
 
     def loop(self):
         self.root.mainloop()
 
+    def printPressedButton(self,event):#действия при нажатии на мыш или клавиатуру
+
+            mouse_x = event.x
+            mouse_y = event.y  #координаты кусора
 
 
 
+            if game_over_flag == False:# если игры не окончилась
+
+                if (tic.draw_Human_sight(mouse_x,mouse_y) != False):# если тыкнули в правильном месте
+                    my_pc.Comp_step(comp_sigh,human_sigh)# комп делает свой ход
 
 
-    def printPressedButton(self,event):
-
-            mouse_x = event.x - (event.x % 10)
-            mouse_y = event.y - (event.y % 10)
-
-
-
-            if game_over_flag == False:
-
-                if (tic.draw_Human_sight(mouse_x,mouse_y) != False):
-                    my_pc.Comp_step(comp_sigh,human_sigh)
-
-                    print(count_steps)
-                    if count_steps >= 9:
+                    if count_steps >= 9:# для того чтобы фиксировать ничью
 
 
                         win.game_over()
 
-    def game_over(self):
+    def game_over(self):# выводит сообщение о окончании игры
         global game_over_flag,count_steps
 
-        canvas = Canvas(bg="black", width=600, height=100)
+        canvas = Canvas(bg="black", width=600, height=100)#надпись "GAME OVER"
         label = ttk.Label(text="GAME OVER!!!", font=("Arial", 30),background="black",foreground="yellow")
 
-        game_over_flag = True
-        count_steps = 0
+        game_over_flag = True # чтобы игрок не мог больше ходить
+        count_steps = 0 # обнуляем счетчик ходов
 
         canvas.place( x = 100, y = 300)
         label.place(x=250, y=320)
-
-
 
 
         new_game_button = Button(win.root,text="Новая игра",command = click_new_game, background="black", fg="yellow", relief=FLAT, cursor="mouse")
@@ -79,28 +74,11 @@ class Window():
         exit_game_button = Button(win.root ,text="Выход", command= click_quit ,background="black", fg="yellow", relief=FLAT, cursor="mouse")
         exit_game_button.place(x=450, y = 420 )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Board():
+class Board():# класс отвечает за игровую доску ,за проверку на победу и ничью ,отрисовку полей поля для GUI
 
     def __init__(self):
 
-        self.board = [0,1,2,3,4,5,6,7,8]
-        self.board_coord_colect = []
+        self.board = [0,1,2,3,4,5,6,7,8]#ячейки игрового поля вместо чисел ставятся знаки игрока и компьютера
         canvas = Canvas(bg="black", width=5, height=100)
         self.borders = canvas.create_line(0, 0, 100, 0,smooth=True,fill="orange",width=10)
         canvas.place(x=50, y=50)
@@ -109,18 +87,7 @@ class Board():
             (0, 3, 6), (1, 4, 7), (2, 5, 8), # вертикальные комбинации
             (0, 4, 8), (2, 4, 6,))           # диагональные комбинции
 
-
-    def init_coord_board(self,height):
-
-        for i in range (100,500,150):
-            for y in range (200,600,150):
-               self.board_coord_colect.append([y,i])
-
-        return self.board_coord_colect
-
-
-
-    def check_winner(self,board):
+    def check_winner(self,board): #метод проверяет на победу или поражение , ничью каждый ход
 
         winner = False
 
@@ -134,28 +101,23 @@ class Board():
 
         return winner
 
+    def init_board_borders(self,height,widht):#отрисовка игрового поля на GUI
+        coords = ((150,225),(150,375),(475,50),(325,50))#координаты линий
+        widht = 500#ширина
+        height = 5#высота
+        count = 0#счетчик  для того чтобы начать рисовать горизонтальные линии
 
+        for i in range(0,len(coords)):
+            if count == 2 :# поворот линий на 90 градусов
+                widht = 5
+                height = 500
 
-    def init_board_borders(self,height,widht):
+            canvas = Canvas(bg="black", width=widht, height=height)
+            self.borders = canvas.create_line(0, 0, 100, 0, smooth=True, fill="black", width=10)
+            canvas.place(x=coords[i][0], y=coords[i][1])
+            count += 1
 
-        canvas = Canvas(bg="black", width=5, height=500)
-        self.borders = canvas.create_line(0, 0, 100, 0, smooth=True, fill="black", width=10)
-        canvas.place(x=(height // 2)+75, y=(widht // 2)-250)
-
-        canvas = Canvas(bg="black", width=5, height=500)
-        self.borders = canvas.create_line(0, 0, 100, 0, smooth=True, fill="black", width=10)
-        canvas.place(x=(height // 2) - 75, y=(widht // 2) - 250)
-
-        canvas = Canvas(bg="black", width=500, height=5)
-        self.borders = canvas.create_line(0, 0, 100, 0, smooth=True, fill="black", width=10)
-        canvas.place(x=150, y=225)
-
-        canvas = Canvas(bg="black", width=500, height=5)
-        self.borders = canvas.create_line(0, 0, 100, 0, smooth=True, fill="black", width=10)
-        canvas.place(x=150, y=375)
-
-
-class TicTac():
+class TicTac():# отрисовка знака игрока
 
 
     def draw_Human_sight(self,mouse_x,mouse_y):
@@ -163,37 +125,40 @@ class TicTac():
 
         canvas = Canvas(bg="black", width=100, height=100)
 
-        self.icon_x = (canvas.create_line(10, 10, 90, 90,smooth=True,fill="orange",width=10),
+        self.icon_x = (canvas.create_line(10, 10, 90, 90,smooth=True,fill="orange",width=10),#крестик состоит из двух пересеченных линий
                        canvas.create_line(10, 90, 90, 10,smooth=True,fill="orange",width=10))
 
-        index = 0
-        step_was_do = False
+        index = 0 #необходим чтобы записать в нужную ячейку знак
+        step_was_do = False# чтобы программа не рисовала знак если игрок тычет куда не надо
 
-        for i in battle_field.board_coord_colect:
-            if i[0] <= mouse_x <= i[0]+100 and i[1] <= mouse_y <= i[1]+100 :
+        for i in board_coord_colect:
+            if i[0] <= mouse_x <= i[0]+100 and i[1] <= mouse_y <= i[1]+100 : # если попал в диапазон координат то рисуем
 
                 canvas.place(x=i[0],y=i[1])
 
                 battle_field.board[index] = 'x'
-                step_was_do = True
+                step_was_do = True#шаг сделан компьютер может ходить
 
             index += 1
 
 
-        if step_was_do == False:
+        if step_was_do == False:#омпьютер ничего неделает
 
             return False
 
         count_steps += 1
 
-        battle_field.check_winner(battle_field.board)
+        battle_field.check_winner(battle_field.board)#проверка на победу
 
-        win.root.update()
+        win.root.update()#бновляем окно
 
 
-class Comp_Enemy():
+class Comp_Enemy():# это бот
+    # работает так : сначало в пустые клетки пробует подставить свой знак ,если победный ход ,то делает его в текущуюю ячейку
+    # иначе он делает тоже самое с знаком игрока
+    # затем пробует поставить в клетки из varius_of_step это лучшие ходы в порядке убывания
     def __init__(self):
-        self.varius_of_step = (4, 0, 2, 6, 8, 1, 3, 5, 7)
+        self.varius_of_step = (4, 0, 2, 6, 8, 1, 3, 5, 7)#учшие ходы в порядке убывания
 
     def Comp_step(self,comp_sign,human_sign):
         global count_steps
@@ -218,7 +183,8 @@ class Comp_Enemy():
                 else:
                     battle_field.board[index] = save_free_cell
 
-        if battle_field.board[5]== human_sigh and battle_field.board [7]  == human_sigh and (fix_me_flag == True) and battle_field.board[8] != human_sigh:
+        if battle_field.board[5]== human_sigh and battle_field.board [7]  == human_sigh and (fix_me_flag == True) \
+                and battle_field.board[8] != human_sigh and battle_field.board[8] != comp_sigh:
             fix_me_flag = False
             self.draw_Comp_sight(8)
             battle_field.board[8] = comp_sigh
@@ -234,14 +200,7 @@ class Comp_Enemy():
 
                 return 0
 
-
-
-
-
-
-
-
-    def draw_Comp_sight(self,index):
+    def draw_Comp_sight(self,index):#отрисовка знака компьютера ,передаем идекс ячейки и берем ей соотвествующие координаты
 
 
 
@@ -249,58 +208,48 @@ class Comp_Enemy():
 
         self.icon_o = canvas.create_oval(10, 10, 90, 90, fill="#80CBC4", outline="#004D40")
 
-        canvas.place(x=battle_field.board_coord_colect[index][0], y=battle_field.board_coord_colect[index][1])
+        canvas.place(x=board_coord_colect[index][0], y=board_coord_colect[index][1])
 
-
-
-
-def click_quit():
+def click_quit():#выход по кнопке
 
     quit()
 
 
-def click_new_game():
+def click_new_game(): #новая игра по кнопке
     global game_over_flag,fix_me_flag,count_steps
 
-    game_over_flag = False
+    game_over_flag = False #приводим в исходное положение флаги
     fix_me_flag = True
     count_steps = 0
 
 
-    main()
+    main()# заново отрисовываем окно игры
 
-
-
-def main():
+def main(): #главный цикл игры
 
     global count_steps
 
-    battle_field.board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    battle_field.board = [0, 1, 2, 3, 4, 5, 6, 7, 8]#обновляем игровую доску
 
-    canvas = tkinter.Canvas(win.root, height=win.height, width=win.width+200)
+    canvas = tkinter.Canvas(win.root, height=win.height, width=win.width+200)#отрисовка фона
     img = tkinter.PhotoImage(file='cosmos.png')
     image = canvas.create_image(-1, -1, anchor='nw', image=img)
     canvas.place(x=0, y=0)
 
-
-    battle_field.init_board_borders(HEIGHT,WIDTH)
-
-
-
-    win.root.bind('<ButtonPress>', win.printPressedButton )
+    battle_field.init_board_borders(HEIGHT,WIDTH)# игровое поле на GUI
 
 
 
-
+    win.root.bind('<ButtonPress>', win.printPressedButton )# действия по нажатию на кнопку.
 
     win.loop()
 
 
-win = Window(HEIGHT,WIDTH)
-battle_field = Board()
-tic = TicTac()
-my_pc = Comp_Enemy()
+win = Window(HEIGHT,WIDTH)#инициализация объектов
+battle_field = Board()#доска
+tic = TicTac()#знаки
+my_pc = Comp_Enemy()#бот
 
-battle_field.init_coord_board(HEIGHT)
 
-main()
+
+main()#входим в главный цикл  
