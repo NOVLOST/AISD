aa8815709cb2d87779f8f1b1e58dfa6ed3247f9a
NOVLOST <159267939+NOVLOST@users.noreply.github.com>
2024-10-29 15:30:31 +0400

    Add files via upload

--- laba11/main.py
+++ laba11/main.py
@@ -1,6 +1,5 @@
 
 import tkinter
-import time
 from tkinter import *
 from tkinter import ttk
 
@@ -9,10 +8,9 @@ from tkinter import ttk
 open_flag = False #отвечает за отрытое положение микроволновки
 work_flag = False #отвечает за рабочее положение микроволновки
 rozet_flag =True  #вкл/выкл вылки из разетки
-time_minute = 0   #кол-во минут на таймере
-time_second = 0   #кол-во секунд на таймере
-minut_string = "" #строки вывода на таймер
-second_string = ""
+time = 0   #время работы храниться в секундах
+power_state = "disable"
+
 
 
 class Window(): # окно на котом все рисуется
@@ -30,7 +28,7 @@ class Window(): # окно на котом все рисуется
         self.win.mainloop()
 
     def printPressedButton(self, event):  # действия при нажатии на мыш или клавиатуру
-        global canvas, img, open_flag,work_flag, rozet_flag,img_rozet
+        global canvas, img, open_flag,work_flag, rozet_flag,img_rozet,power_state,reset_time,add_second,add_minute,start
 
         #
         # Close/Open
@@ -48,26 +46,6 @@ class Window(): # окно на котом все рисуется
 
             open_flag = False
 
-
-        #
-        # ON/OFF Eсли розетка не воткнута то работать печь не будет
-        #
-        elif 450 <= event.x <= 600 and 10 <= event.y <= 250 and (work_flag == False and rozet_flag == False) :
-
-             # отрисовка фона
-            img = PhotoImage(file='microwave_work.png')
-            canvas.create_image(-1, -1, anchor='nw', image=img)
-
-            work_flag = True
-
-        elif 450 <= event.x <= 600 and 10 <= event.y <= 250 and work_flag == True:
-
-              # отрисовка фона
-            img = PhotoImage(file='microwave_close.png')
-            canvas.create_image(-1, -1, anchor='nw', image=img)
-
-            work_flag = False
-
         #
         # Розетка вкл/выкл
         #
@@ -78,7 +56,13 @@ class Window(): # окно на котом все рисуется
             img_rozet = PhotoImage(file='rozet.png')
             canvas.create_image(600, 10, anchor='nw', image=img_rozet)
 
+            # вкл/выкл кнопок взависимотси от состояния розетки
             rozet_flag = False
+            reset_time['state']= tkinter.NORMAL
+            start['state'] = tkinter.NORMAL
+            add_second['state'] = tkinter.NORMAL
+            add_minute['state'] = tkinter.NORMAL
+
 
         elif 750 <= event.x <= 900 and 30 <= event.y <= 150 and rozet_flag == False:
 
@@ -88,113 +72,124 @@ class Window(): # окно на котом все рисуется
             img = PhotoImage(file='microwave_close.png')
             canvas.create_image(-1, -1, anchor='nw', image=img)
 
+
+            #вкл/выкл кнопок взависимотси от состояния розетки
             rozet_flag = True
             work_flag = False
+            reset_time['state']= tkinter.DISABLED
+            start['state'] = tkinter.DISABLED
+            add_second['state'] = tkinter.DISABLED
+            add_minute['state'] = tkinter.DISABLED
     #---------------------------
-    #ЛОГИКА ТАЙМЕРА ПО МИНУТАМ
+    #ЛОГИКА ТАЙМЕРА ПО МИНУТАМ И СЕКУНДАМ
     #---------------------------
-    def logic_timer_minute(self,time):
-        global time_minute,minut_string
+    def logic_timer(self,minute,seconds):
 
-        if 0 < time < 10:
-            minut_string = f"0{time_minute}"
-        elif 0 < time < 100:
-            minut_string = f"{time_minute}"
-        elif time < 0: # чтобы не выдавало -1 на таймере
 
-            minut_string = f"00"
+        #логика минут
+        if 0 < minute < 10:
+            minut_string = f"0{minute}"
+
+        elif 0 < minute < 100:
+            minut_string = f"{minute}"
 
         else:# когда кол-во минут большее 100
-            time_minute = 0
-            minut_string = f"0{time_minute}"
+            minute = 0
+            minut_string = f"0{minute}"
+
+
+
+
+
+        #логика секунд
+        if 10 <= seconds < 60:
+            second_string = f"{seconds}"
+
+        elif seconds < 10:
+            second_string = f"0{seconds}"
+
+        else : #чтобы секунды больше 60 переводить в минуту
+            seconds = 0
+            second_string = f"0{seconds}"
+
+
+        #отображение секунд
+        second_label = Label(win.win, text=f': {second_string}', fg="orange", bg='black', relief=FLAT, bd='5',font=("Arial", 20))
+        second_label.place(x=520, y=40)
 
         #отображение минут
-        minute = Label(win.win, text=minut_string, fg="orange", bg='black', relief=FLAT, bd='5', font=("Arial", 20))
-        minute.place(x=480, y=40)
+        minute_label = Label(win.win, text=minut_string, fg="orange", bg='black', relief=FLAT, bd='5', font=("Arial", 20))
+        minute_label.place(x=480, y=40)
         self.win.update()
 
     def timer_minute(self): # функция прибавления времени
-        global time_minute,minut_string
+        global time
 
-        time_minute += 1
-
-        self.logic_timer_minute(time_minute)
-
-    # ---------------------------
-    # ЛОГИКА ТАЙМЕРА ПО СЕКУНДАМ
-    # ---------------------------
-    def logic_timer_second(self,time):#
-        global time_second, second_string
+        time += 60
+        #первый аргумент минуты второй секунды
+        self.logic_timer(time // 60,time - ((time // 60) * 60))
 
 
-        if 10 <= time < 60:
-            second_string = f"{time_second}"
-
-        elif time < 0: # чтобы не выдавало -1 на таймере
-            time_second = 0
-            second_string = f"0{time_second}"
-
-        elif time < 10:
-            second_string = f"0{time_second}"
-
-        else : #чтобы секунды больше 60 переводить в минуту
-            win.timer_minute()
-            time_second = 0
-            second_string = f"0{time_second}"
-
-
-
-        second = Label(win.win, text=f': {second_string}', fg="orange", bg='black', relief=FLAT, bd='5',font=("Arial", 20))
-        second.place(x=520, y=40)
-        self.win.update()
 
 
 
     def timer_second(self):
-        global time_second, second_string
+        global time
 
-        time_second += 10
+        time += 10
 
-        self.logic_timer_second(time_second)
+        self.logic_timer(time // 60,time - ((time // 60) * 60))
 
-    #если секунды < 0 отнимаем 1 минуту и смотрим, если < 0 минут осталось то выходим иначе отсчитываем секунды дальше
+    #---------------------------------------------------------------
+    #СЧЕТЧИК:отнимаем секунду и отправляем минуты и секунды на обработку
+    #------------------------------------------------------------------
     def countdown(self):
-        global time_second,time_minute
+        global time,img,canvas,open_flag
 
-        while (time_minute > 0 or time_second > 0):
-            time_second -= 1
-            self.win.after(1000,self.logic_timer_second(time_second))
-            if time_second <= 0:
-                time_minute -= 1
-                self.win.after(1000, self.logic_timer_minute(time_minute))
-                if time_minute >= 0:
-                    time_second = 59
-                    self.win.after(1000, self.logic_timer_second(time_second))
+        if (work_flag == False and rozet_flag == False):
+            open_flag = False
+            img = PhotoImage(file='microwave_work.png')
+            canvas.create_image(-1, -1, anchor='nw', image=img)
 
-                else:
-                    time_second = 0
-                    self.win.after(1000, self.logic_timer_second(time_second))
-                    
+
+            while ( time > 0 ) and (rozet_flag == False) and (open_flag == False):
+
+                time -= 1
+                min = time // 60
+                sec = time - (min * 60)
+                self.win.after(1000, self.logic_timer(min,sec))
+
+            if open_flag == False:
+                img = PhotoImage(file='microwave_close.png')
+                canvas.create_image(-1, -1, anchor='nw', image=img)
+
+
+
+
 
     #кнопка сброс
     def reset(self):
-        global time_second, time_minute
+        global time,work_flag,img,canvas
 
-        time_second = 0
-        time_minute = 0
+        time = 0
 
-        second = Label(win.win, text=': 00', fg="orange", bg='black', relief=FLAT, bd='5',font=("Arial", 20))
-        second.place(x=520, y=40)
+        second_label = Label(win.win, text=': 00', fg="orange", bg='black', relief=FLAT, bd='5',font=("Arial", 20))
+        second_label.place(x=520, y=40)
 
-        minute = Label(win.win, text='00', fg="orange", bg='black', relief=FLAT, bd='5', font=("Arial", 20))
-        minute.place(x=480, y=40)
+        minute_label = Label(win.win, text='00', fg="orange", bg='black', relief=FLAT, bd='5', font=("Arial", 20))
+        minute_label.place(x=480, y=40)
+
+        img = PhotoImage(file='microwave_close.png')
+        canvas.create_image(-1, -1, anchor='nw', image=img)
+
+        work_flag = False
 
 
 
 
 win = Window()
 
-# необязательно каждый раз рисовать canvas можно просто вызывать create_image и лепать на старом canvas
+# необязательно каждый раз рисовать сanvas можно просто вызывать create_image и лепать на старом canvas
 
 canvas = Canvas(win.win, height=win.width, width=win.height)  # отрисовка фона
 img = PhotoImage(file='microwave_close.png')
@@ -204,17 +199,17 @@ img_rozet = PhotoImage(file='rozet_off.png') # отрисовка розетки
 canvas.create_image(600, 10, anchor='nw', image=img_rozet)
 
 #отрисовка дисплея таймера
-minute = Label(win.win, text='00',fg = "orange",bg = 'black',relief=FLAT,bd='5',font=("Arial", 20))
-second = Label(win.win, text=': 00',fg = "orange",bg = 'black',relief=FLAT,bd='5',font=("Arial", 20))
+minute_label = Label(win.win, text='00',fg = "orange",bg = 'black',relief=FLAT,bd='5',font=("Arial", 20))
+second_label = Label(win.win, text=': 00',fg = "orange",bg = 'black',relief=FLAT,bd='5',font=("Arial", 20))
 
 #кнопки добавления минут,секунд сброс и пуск
-add_minute = ttk.Button(win.win,text = "+1 мин",cursor="top_left_corner",width=6,command=win.timer_minute)
-add_second = ttk.Button(win.win,text = "+10 сек",cursor="top_left_corner",width=6 ,command=win.timer_second)
-reset_time = ttk.Button(win.win,text = "сброс",cursor = "top_left_corner",width = 6,command=win.reset)
-start = ttk.Button(win.win,text = "пуск",cursor="top_left_corner",width=6 ,command=win.countdown)
+add_minute = ttk.Button(win.win,text = "+1 мин",state = tkinter.DISABLED,cursor="top_left_corner",width=6,command=win.timer_minute)
+add_second = ttk.Button(win.win,text = "+10 сек",state = tkinter.DISABLED,cursor="top_left_corner",width=6 ,command=win.timer_second)
+reset_time = ttk.Button(win.win,text = "сброс",state = tkinter.DISABLED,cursor = "top_left_corner",width = 6,command=win.reset)
+start = ttk.Button(win.win,text = "пуск",state = tkinter.DISABLED,cursor="top_left_corner",width=6 ,command=win.countdown)
 
-minute.place(x = 480 , y = 40)
-second.place(x = 520 , y = 40 )
+minute_label.place(x = 480 , y = 40)
+second_label.place(x = 520 , y = 40 )
 reset_time.place(x = 530 , y = 150  )
 start.place (x = 460, y = 150)
 add_minute.place(x = 460,y=200 )
